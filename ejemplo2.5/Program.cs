4d417b5f49d288933c071218f5807fb015ea27cc
SANTIAGO <SANTIAGO@LAPTOP-REHH1RB2>
2022-11-13 18:53:37 -0600

    Agregar archivos de proyecto.

---
+++ ejemplo2.5/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ejercicio3
+{
+    public class sueldo
+    {
+        public static void Main()
+        {
+            int ss, ch, vh;
+            Console.WriteLine("digite las horas trabajadas");
+            ch=int.Parse(Console.ReadLine());
+
+            Console.WriteLine("ingrese el valor de la hora");
+            vh=int.Parse(Console.ReadLine());
+
+            ss = ch * vh;
+            Console.WriteLine($"el sueldo semanal es de {ss}");
+        }
+    }
+}
